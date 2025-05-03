9799247581d2d11cc0343dbde52594eb7a06b861
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2025-05-03 16:27:24 -0300

    🔐  Added key creation

--- crypto/index.js
+++ crypto/index.js
@@ -1,4 +1,7 @@
 import Crypto from "./crypto.js";
+// import Key from './key.js';
+
+// Key.createKey(32);
 
 const text = 'Yves, I\'m here to help you with your code!';
 console.log('Original:', text);
