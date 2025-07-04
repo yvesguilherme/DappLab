3a509d49538bba5fa0dfa7d3074e178bf6f400a4
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2025-07-03 21:13:54 -0300

    ✨ Generation of eec key pair and addition of acpair and tiny-secp256k1 libraries

---
+++ protochain/src/client/wallet-client.ts
@@ -0,0 +1,4 @@
+import Wallet from "../lib/wallet.ts";
+
+const wallet = new Wallet('5826c9a6e6921e7d9c6e9d39106e3b160f23a910df96ce13d45cf1afd4103871');
+console.log(wallet);
---
+++ protochain/src/lib/wallet.ts
@@ -0,0 +1,38 @@
+import * as ecc from 'tiny-secp256k1';
+import ECPairFactory, { ECPairInterface } from 'ecpair';
+
+const ECPair = ECPairFactory(ecc);
+
+/**
+ * Wallet class for managing cryptocurrency wallets.
+ */
+class Wallet {
+  privateKey: string;
+  publicKey: string;
+
+  constructor(wifOrPrivateKey?: string) {
+    let keys;
+
+    if (wifOrPrivateKey) {
+      try {
+        if (wifOrPrivateKey.length === 64) {
+          // If a private key is provided
+          keys = ECPair.fromPrivateKey(Buffer.from(wifOrPrivateKey, 'hex'));
+        } else {
+          // If a WIF (Wallet Import Format) is provided
+          keys = ECPair.fromWIF(wifOrPrivateKey);
+        }
+      } catch (error) {
+        const message = (error instanceof Error) ? error.message : error;
+        throw new Error(`Invalid WIF or private key: ${message}`);
+      }
+    } else {
+      keys = ECPair.makeRandom();
+    }
+
+    this.privateKey = keys.privateKey ? Buffer.from(keys.privateKey).toString('hex') : '';
+    this.publicKey = Buffer.from(keys.publicKey).toString('hex');
+  }
+}
+
+export default Wallet;
