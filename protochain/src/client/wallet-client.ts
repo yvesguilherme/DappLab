4b471d28dd8e58b0ca53392a00e1cbe7fd34a919
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2025-07-09 23:53:04 -0300

    ✨  Added a client that generates and retrieves wallets

--- protochain/src/client/wallet-client.ts
+++ protochain/src/client/wallet-client.ts
@@ -1,4 +1,126 @@
+import readline from 'readline';
+
+import axios from 'axios';
+
+import configEnv from "../config/config-env.ts";
 import Wallet from "../lib/wallet.ts";
 
-const wallet = new Wallet('5826c9a6e6921e7d9c6e9d39106e3b160f23a910df96ce13d45cf1afd4103871');
-console.log(wallet);
+const BLOCKCHAIN_SERVER = configEnv.BLOCKCHAIN_SERVER;
+let myWalletPub = '';
+let myWalletPriv = '';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function menu() {
+  setTimeout(() => {
+    console.clear();
+
+    console.log('Welcome to the Blockchain Wallet Client!');
+
+    if (myWalletPub) {
+      console.log(`You're logged as ${myWalletPub}`);
+    } else {
+      console.log('You are not logged in.');
+    }
+
+    console.log('1 - Create a new wallet');
+    console.log('2 - Recover wallet');
+    console.log('3 - Balance');
+    console.log('4 - Send tx');
+    console.log('5 - Exit');
+
+    rl.question('Choose your option: ', (answer) => {
+      switch (answer) {
+        case '1':
+          createWallet();
+          break;
+        case '2':
+          recoverWallet();
+          break;
+        case '3':
+          getBalance();
+          break;
+        case '4':
+          sendTransaction();
+          break;
+        case '5':
+          exitProgram();
+          break;
+        default:
+          console.log('Invalid option, please try again.');
+          menu();
+      }
+    });
+  }, 1000);
+}
+
+function preMenu() {
+  rl.question('Press any key to continue...', () => menu());
+}
+
+function createWallet() {
+  console.clear();
+
+  const wallet = new Wallet();
+
+  console.log('Creating a new wallet...');
+  console.log(`Your new wallet:`, wallet);
+
+  myWalletPub = wallet.publicKey;
+  myWalletPriv = wallet.privateKey;
+
+  preMenu();
+}
+
+function recoverWallet() { 
+  console.clear();
+
+  rl.question(`What's your private key or WIF? `, (privateKeyOrWif) => {
+    const wallet = new Wallet(privateKeyOrWif);
+
+    console.log('Recovering your wallet...');
+    console.log(`Your recovered wallet:`, wallet);
+
+    myWalletPub = wallet.publicKey;
+    myWalletPriv = wallet.privateKey;
+    preMenu();
+  });
+}
+
+function getBalance() {
+  console.clear();
+
+  if (!myWalletPub) {
+    console.log(`You need to create or recover a wallet first.`);
+    preMenu();
+    return;
+  }
+
+  // TODO: Get balance from the blockchain server
+  preMenu();
+ }
+
+function sendTransaction() { 
+  console.clear();
+
+  if (!myWalletPub) {
+    console.log(`You need to create or recover a wallet first.`);
+    preMenu();
+    return;
+  }
+
+  // TODO: Send tx from the blockchain server
+  preMenu();
+}
+
+function exitProgram() {
+  console.clear();
+  console.log('Exiting the wallet client. Goodbye!');
+  rl.close();
+  process.exit(0);
+}
+
+menu();
